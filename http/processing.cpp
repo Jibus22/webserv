f2b18778d7a51db56e7bef6b48a2f097f7f4d2a9
mouchy <mrouchy@student.42.fr>
2021-08-19 11:57:32 +0200

    ✨ feat(http): ajout de processing pour lien entre partie core et http processing

---
+++ http/processing.cpp
@@ -0,0 +1,13 @@
+/*
+Response const * process_request(std::string const & raw_request,
+									Config_struct const & config)
+{
+	Request r = Request(raw_request);
+
+	Response *response = new Response();
+
+	//processing
+
+	return response;
+}
+*/
