3f97291d744ecfaf4d9273e868113520a6d3a4cb
mouchy <mrouchy@student.42.fr>
2021-08-23 17:40:02 +0200

    ✨ feat(http): debut de l'algo de processing

--- http/processing.cpp
+++ http/processing.cpp
@@ -1,13 +1,27 @@
-/*
-Response const * process_request(std::string const & raw_request,
-									Config_struct const & config)
+#include "processing.hpp"
+#include <iostream>
+#include <string>
+
+std::string const *process_request(std::string& raw_request,
+	const std::vector<Server>& server_blocks,
+	const std::pair<std::string, int>& listen, int *flag)
 {
-	Request r = Request(raw_request);
+	try {
+		Request r = Request(raw_request);
 
-	Response *response = new Response();
-
-	//processing
-
-	return response;
+		// On recupere le serveur associe a la requete
+		Server & s = match_server(server_blocks, listen, r);
+		Response response;
+		return response.get_raw();
+	}
+	catch (Request::NotTerminatedException e)
+	{
+		//Requete non termine set flag
+		return NULL;
+	}
+	catch (Request::InvalidRequest e)
+	{
+		//Requete non valide set flag
+		return NULL;
+	}
 }
-*/
