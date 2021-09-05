734e5ec5be58fccbf9f5dfa9e05154bc7fa5ece6
Le Corre <jibeguitare@hotmail.fr>
2021-09-05 17:53:33 +0200

    🚧 wip(cgi): mix cgiMetaVar into CgiEnv. Only one obj now. Add setEnv()

--- cgi/CgiEnv.cpp
+++ cgi/CgiEnv.cpp
@@ -3,10 +3,15 @@
 
 /*------------------------CONSTRUCTOR / DESTRUCTOR----------------------------*/
 
-CgiEnv::CgiEnv(void)
-{}
+CgiEnv::CgiEnv(void) : _metaVar(METAVAR_NB)
+{
+	initMetaVar();
+}
 
-CgiEnv::CgiEnv(CgiEnv const & src) { *this = src; }
+CgiEnv::CgiEnv(CgiEnv const & src) : _metaVar(src._metaVar)
+{
+	setEnv();
+}
 
 CgiEnv::~CgiEnv()
 {}
@@ -15,53 +20,77 @@ CgiEnv &	CgiEnv::operator=(CgiEnv const & src)
 {
 	if (this == &src)
 		return *this;
+	_metaVar = src._metaVar;
+	setEnv();
 	return *this;
 }
 
 
 /*----------------------------------METHODS-----------------------------------*/
 
+void	CgiEnv::initMetaVar()
+{
+	_metaVar[SERVER_PROTOCOL] = "SERVER_PROTOCOL = HTTP/1.1";
+	_metaVar[SERVER_SOFTWARE] = "SERVER_SOFTWARE = webserv";
+	_metaVar[GATEWAY_INTERFACE] = "GATEWAY_INTERFACE = CGI/1.1";
+	_metaVar[CONTENT_LENGHT] = "CONTENT_LENGHT = ";
+	_metaVar[CONTENT_TYPE] = "CONTENT_TYPE = ";
+	_metaVar[PATH_INFO] = "PATH_INFO = ";
+	_metaVar[QUERY_STRING] = "QUERY_STRING = ";
+	_metaVar[REQUEST_METHOD] = "REQUEST_METHOD = ";
+	_metaVar[REMOTE_ADDR] = "REMOTE_ADDR = ";
+	_metaVar[SCRIPT_NAME] = "SCRIPT_NAME = ";
+	_metaVar[SERVER_NAME] = "SERVER_NAME = ";
+	_metaVar[SERVER_PORT] = "SERVER_PORT = ";
+}
+
+void	CgiEnv::setEnv()
+{
+	for (int i = 0; i  < METAVAR_NB; i++)
+		env[i] = _metaVar[i].c_str();
+}
 
 
 /*------------------------------GETTERS/SETTERS-------------------------------*/
 
-	// /foo/test.php/path/info?query=string 
-	//
-	// SCRIPTNAME->/foo/test.php
-	//trouver extension cgi dans target et ajouter a metavar
-	//
-	//PATH_INFO -> /var/www/website/users/path/info
-	//avec root /var/www/website/users/
-	//extraire path/info de la target http et la rajouter à root
+// /foo/test.php/path/info?query=string 
+//
+// SCRIPTNAME->/foo/test.php
+//trouver extension cgi dans target et ajouter a metavar
+//
+//PATH_INFO -> /var/www/website/users/path/info
+//avec root /var/www/website/users/
+//extraire path/info de la target http et la rajouter à root
 
 void	CgiEnv::setMetaVar(const Request& request,
-					const Location_config& location_block
-					const Server_config& server_block)
+					const Location_config& location_block,
+					const Server_config& server_block,
 					const Client& client)
 {
 	size_t				find, find2;
 	const std::string&	str;
 
-	_metaVar._request_method.append(request.get_method());//REQUEST_METHOD
+	//REQUEST_METHOD
+	_metaVar[REQUEST_METHOD].append(request.get_method());
 
 	str = request.get_header("content-lenght");//CONTENT_LENGTH
 	if (str.size() > 0)
-		_metaVar._content_length.append(str);
+		_metaVar[CONTENT_LENGTH].append(str);
 
 	str = request.get_header("content-type");//CONTENT_TYPE
 	if (str.size() > 0)
-		_metaVar._content_type.append(str);
+		_metaVar[CONTENT_TYPE].append(str);
 
 	str = request.get_target();
 	find = str.find_first_of('?');//QUERY_STRING
 	if (find != std::string::npos && (find + 1) <= str.size())
-		_metaVar._query_string.append(str, find + 1, std::string::npos);
+		_metaVar[QUERY_STRING].append(str, find + 1, std::string::npos);
 
 	find = str.find(request.getCgi());//SCRIPT_NAME
 	if (find != std::string::npos)
 	{
 		find += (request.getCgi()).size();
-		_metaVar._script_name.append(str, 0, find - 1);
+		_metaVar[SCRIPT_NAME].append(str, 0, find - 1);
 	}
 
 	find2 = find;//PATH_INFO
@@ -69,23 +98,46 @@ void	CgiEnv::setMetaVar(const Request& request,
 		find2++;
 	if ((find2 - find) > 1 && find2 < str.size())
 	{
-		_metaVar._path_info.append(location_block.root);
-		_metaVar._path_info.append(str, find, find2);
+		_metaVar[PATH_INFO].append(location_block.root);
+		_metaVar[PATH_INFO].append(str, find, find2);
 	}
 
 	if (server_block.name_serv.empty() == false)//SERVER_NAME
-		_metaVar._server_name.append(server_block.name_serv[0]);
+		_metaVar[SERVER_NAME].append(server_block.name_serv[0]);
 	else
-		_metaVar._server_name.append((client.getListen())->first);
+		_metaVar[SERVER_NAME].append((client.getListen())->first);
 
-	_metaVar._server_port.append((client.getListen())->second);//SERVER_PORT
+	//SERVER_PORT
+	_metaVar[SERVER_PORT].append((client.getListen())->second);
 
-	_metaVar._server_port.append((client.getRemoteAddr()));//REMOTE_ADDR
+	//REMOTE_ADDR
+	_metaVar[SERVER_PORT].append((client.getRemoteAddr()));
+
+	setEnv();
 }
+
+const char	*CgiEnv::getEnv(void) const { return _env; }
+
+const std::vector<std::string>&
+			CgiEnv::getMV(void) const { return _metaVar; }
 
 std::ostream &operator<<(std::ostream &out, CgiEnv const &value)
 {
-	//out << "I am " << value.getName() << std::endl;
+	const std::vector<std::string>&	mv = value.getMV();
+
+	out
+	<< mv[SERVER_PROTOCOL] << std::endl
+	<< mv[SERVER_SOFTWARE] << std::endl
+	<< mv[GATEWAY_INTERFACE] << std::endl
+	<< mv[CONTENT_LENGHT] << std::endl
+	<< mv[CONTENT_TYPE] << std::endl
+	<< mv[PATH_INFO] << std::endl
+	<< mv[QUERY_STRING] << std::endl
+	<< mv[REQUEST_METHOD] << std::endl
+	<< mv[REMOTE_ADDR] << std::endl
+	<< mv[SCRIPT_NAME] << std::endl
+	<< mv[SERVER_NAME] << std::endl
+	<< mv[SERVER_PORT] << std::endl;
 	return (out);
 }
 
--- cgi/includes/CgiEnv.hpp
+++ cgi/includes/CgiEnv.hpp
@@ -2,21 +2,52 @@
 #define CGIENV_HPP
 
 #include "Request.hpp"
+#include "Client.hpp"
 #include "CgiMetaVar.hpp"
 #include <string>
 #include <vector>
 
+# define METAVAR_NB 12
+
+# define SERVER_PROTOCOL 0
+# define SERVER_SOFTWARE 1
+# define GATEWAY_INTERFACE 2
+# define CONTENT_LENGHT 3
+# define CONTENT_TYPE 4
+# define PATH_INFO 5
+# define QUERY_STRING 6
+# define REQUEST_METHOD 7
+# define REMOTE_ADDR 8
+# define SCRIPT_NAME 9
+# define SERVER_NAME 10
+# define SERVER_PORT 11
+
 class CgiEnv
 {
-	CgiMetaVar					_metaVar;
-	std::vector<std::string*>	_env;
+	std::vector<std::string>	_metaVar;
+	const char					*_env[METAVAR_NB];
+
 public:
 	CgiEnv();
 	CgiEnv(const CgiEnv& src);
+	CgiEnv(const Request& request,
+					const Location_config& location_block,
+					const Server_config& server_block,
+					const Client& client);
 	~CgiEnv();
 	CgiEnv&	operator=(const CgiEnv& src);
 
-	void	setMetaVar(const Request& request);
+	void		setMetaVar(const Request& request,
+					const Location_config& location_block,
+					const Server_config& server_block,
+					const Client& client);
+
+	const char						*getEnv() const;
+	const std::vector<std::string>&	getMV() const;
+
+private:
+	void		initMetaVar(void);
+	void		setEnv(void);
 };
 
 std::ostream &operator<<(std::ostream &out, CgiEnv const &value);
